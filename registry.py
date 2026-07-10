78aa1a9ec6f5739c60ec1c9d419e7555f317f127
AdityaKURMI <adityapatelsagar237@gmail.com>
2026-07-10 23:57:52 +0530

    game

--- registry.py
+++ registry.py
@@ -8,11 +8,12 @@ UUID from the URL (data.medicaid.gov/dataset/<UUID>).
 """
 
 SDUD_DATASETS: dict[int, str] = {
+    2020: "cc318bfb-a9b2-55f3-a924-d47376b32ea3",
+    2021: "eec7fbe6-c4c4-5915-b3d0-be5828ef4e9d",
     2022: "200c2cba-e58d-4a95-aa60-14b99736808d",
     2023: "d890d3a9-6b00-43fd-8b31-fcba4c8e2909",
     2024: "61729e5a-7aa8-448c-8903-ba3e0cd0ea3c",
     2025: "158a1baa-5506-400a-8ec3-97756f0b0536",
-    # TODO(you): add 2018-2021 by looking up their UUIDs on data.medicaid.gov
 }
 
 # Start with a small, deliberately diverse set of states:
